05d66cfef015a5179f686e4b5f8f46d4f67a237b
devanshsoni080 <devanshsoni080@gmail.com>
2026-05-11 09:34:59 +0530

    ok

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
 <?php
-
-    class calulation{
-
-            public $a ,  $b , $c;
-
-            function sum(){
-                $this->c = $this->a + $this->b;
-                return $this->c;
-            }
-        
-
-    }
+echo "hello";
 ?>
